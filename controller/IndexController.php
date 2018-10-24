33c3d168b3fdfe05a13f79ad91f51d1c258bc344
Huong <admin@Apples-MacBook-Pro.local>
2018-10-24 21:12:17 +0700

    product

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -1,10 +1,16 @@
 <?php
 include_once 'BaseController.php';
+include_once 'model/IndexModel.php';
 
 class IndexController extends BaseController{
     
     function getHomePage(){
-        $data = [];
+        $model = new IndexModel();
+        $slide = $model->selectSlide();
+        
+        $data = [
+            'slide' => $slide
+        ];
         return $this->loadView('index',$data);
     }
 }
