0a61878fbc29a3da20429f4cc37dbf8e72b5a72f
Huong <admin@Apples-MacBook-Pro.local>
2018-11-02 21:30:30 +0700

    selectCategories

--- controller/TypeProductController.php
+++ controller/TypeProductController.php
@@ -1,10 +1,23 @@
 <?php
 include 'BaseController.php';
+include_once 'model/TypeProductModel.php';
 
 class TypeProductController extends BaseController{
     function getTypePage(){
 
         $urlType =  $_GET['url'];
+        $model = new TypeProductModel;
+        // c1
+        // $type = $model->selectCategoriesByUrl($urlType);
+
+        //c2
+        $type = $model->selectCategories($urlType);
+        if(!$type){
+            header('Location:404.html');
+            return;
+        }
+        // get product by type
+
         return $this->loadView('type-product');
     }
 }
--- model/TypeProductModel.php
+++ model/TypeProductModel.php
@@ -3,12 +3,25 @@ include_once 'DBConnect.php';
 
 class TypeProductModel extends DBConnect{
 
-    function selectCategories(){
+    function selectCategories($url = null){
         $sql = "SELECT c.*, u.url as url 
                 FROM categories c
                 INNER JOIN page_url u
                 ON c.id_url = u.id";
+        if($url != null){
+            $sql .= " WHERE url='$url'";
+            return $this->loadOneRow($sql);
+        }
         return $this->loadMoreRow($sql);
+    }
+
+    function selectCategoriesByUrl($url){
+        $sql = "SELECT c.*
+                FROM categories c
+                INNER JOIN page_url u
+                ON c.id_url = u.id
+                WHERE url='$url'";
+        return $this->loadOneRow($sql);
     }
 }
 
